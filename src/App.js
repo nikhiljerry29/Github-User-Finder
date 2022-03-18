7abc97f2cec1fd6bf8672756730c30efa17b648f
nikhiljerry29 <nikhil.gupta5667@gmail.com>
2022-03-18 16:28:40 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/layouts/Footer";
+import Navbar from "./components/layouts/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Router>
+         <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
+
+            <main className='container mx-auto px-3'>
+               <Routes>
+                  <Route exact path='/' element={<Home />} />
+                  <Route path='/about' element={<About />} />
+                  <Route path='/*' element={<NotFound />} />
+               </Routes>
+            </main>
+
+            <Footer />
+         </div>
+      </Router>
+   );
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function About() {
+   return (
+      <div>
+         <h1 className='text-6xl mb-4'>
+            Github
+            <span className='text-yellow-500'>Finder</span>
+         </h1>
+         <p className='mb-4 text-2xl font-light'>
+            A React app to search GitHub profiles and see profile details. This
+            project is part of the{" "}
+            <a
+               href='https://www.udemy.com/course/modern-react-front-to-back/'
+               className='underline decoration-yellow-500 text-white hover:text-gray-200'
+               target='__blank'
+            >
+               React Front To Back
+            </a>{" "}
+            Udemy course by
+            <strong>
+               <a href='https://traversymedia.com' target='__blank'>
+                  {" "}
+                  Brad Traversy
+               </a>
+            </strong>
+            .
+         </p>
+         <p className='text-lg text-gray-400'>
+            Version <span className='text-white'>1.0.0</span>
+         </p>
+         <p className='text-lg text-gray-400'>
+            Layout By:{" "}
+            <a
+               className='text-white'
+               href='https://twitter.com/hassibmoddasser'
+               target='__blank'
+            >
+               Hassib Moddasser
+            </a>
+         </p>
+      </div>
+   );
+}
+
+export default About;
